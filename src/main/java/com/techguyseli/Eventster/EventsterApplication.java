55da27c5a98f63b3dec3db26bd2ceeec92f81466
techguyseli <kassouselaiman@gmail.com>
2023-11-13 15:45:20 +0100

    first commit

---
+++ src/main/java/com/techguyseli/Eventster/EventsterApplication.java
@@ -0,0 +1,13 @@
+package com.techguyseli.Eventster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsterApplication.class, args);
+	}
+
+}
